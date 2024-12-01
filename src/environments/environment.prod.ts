687ed0e524edc91916a12a02ed985bfc4f5d93c0
anju006 <developeranju006@gmail.com>
2024-12-02 00:43:19 +0530

    page logo icon update

--- src/environments/environment.prod.ts
+++
@@ -1,5 +0,0 @@
-export const environment = {
-    production: true,
-    // apiUrl: 'https://staging-api.example.com',
-    // Add other environment-specific variables here
-  };
--- src/environments/environment.ts
+++
@@ -1,5 +0,0 @@
-export const environment = {
-    production: false,
-    // apiUrl: 'https://staging-api.example.com',
-    // Add other environment-specific variables here
-  };
